db97a1ab3ad16b398bf56f99fb3b00df27993b2c
Kavita Pawar <79410277+kavitapawar4399@users.noreply.github.com>
2023-06-28 16:29:12 +0530

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import '@/styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+    return ( <
+        >
+        <
+        Header / >
+        <
+        div className = "container mx-auto" > < /div> <
+        Component {...pageProps }
+        /> <
+        Footer / >
+        <
+        />
+    )
+
+}
